436989e396c1ca453027cf683f337c402e0e75fd
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-06-25 09:20:38 -0700

    Create 10mindelay.ps1

---
+++ 10mindelay.ps1
@@ -0,0 +1,9 @@
+# Some initial operations
+Write-Output "Starting initial operations..."
+
+# Delay for 10 minutes
+Write-Output "Pausing for 10 minutes..."
+Start-Sleep -Seconds 600
+
+# Continue with the rest of the script
+Write-Output "Resuming operations..."
